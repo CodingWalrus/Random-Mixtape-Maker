ddce6bd6a0b392f0576f81e20dc9389f49d60a45
James Wash <jamescwash2@hotmail.com>
2018-05-06 18:50:05 -0400

    Began genre inclusion scripting - NOT FINISHED

--- mixtape.py
+++ mixtape.py
@@ -1,9 +1,9 @@
 #! python3
-# 
+#
 # Random Mixtape Maker - a computer program to generate randomized playlists
 # from your music library. I saw something like this once on Donation Coder,
 # but I can't find it anymore, and that one wasn't open source anyways. So I
-# decided to make my own. 
+# decided to make my own.
 #
 # This program can only be run in the command line currently.
 #
@@ -19,13 +19,13 @@ from tinytag import TinyTag
 # Create parser for command line arguments
 parser = argparse.ArgumentParser(description='Finds songs in a directory and adds them to a random playlist.')
 # Directory to search for sound files
-parser.add_argument('directory',default='.\\', 
+parser.add_argument('-d',required=True,
                     help='Path to top-level directory to search through for music')
 # Name of playlist to create
-parser.add_argument('playlist_name',default='playlist',
+parser.add_argument('-playlist',required=True,
 					help='Specifies the name of the created playlist')
 # Includes sound file extensions to search for
-parser.add_argument('file_extensions',default='mp3', nargs = '+',
+parser.add_argument('-ext',default='mp3', nargs = '*',
 					help='Specifies the file extensions that can be added to the playlist, single spaced')
 # Sets limit for playlist length by filesize of songs
 parser.add_argument('-file_size',type=int,default=2000000000000,
@@ -36,15 +36,18 @@ parser.add_argument('-time_length',type=int,default=604800,
 # Sets limit for playlist length by song number
 parser.add_argument('-track_nums',type=int,default=0,
 					help='Sets the maximum number of tracks for the playlist')
+# Genre inclusion function
+parser.add_argument('-genre_include',type=str, nargs='*',
+					help='Determines which genres should be included in the playlist')
 # Recursive search setting
 parser.add_argument('-query',default='q',choices = ['y','Y','n','N','q','Q'],
                     help='Determines if the program searches subfolders or not: Y - search subfolders | N - do not subfolders | Q - ask in program')
-					
+
 args = parser.parse_args()
 
 # Main sound file search function
 def song_search(folder, recursive, extensions):
-	# Queries for search setting if not provided in arguments 
+	# Queries for search setting if not provided in arguments
 	if recursive == 'q':
 		print('Would you like to search the subfolders included in this folder? [Y/N]')
 		response = input('>> ')
@@ -52,10 +55,10 @@ def song_search(folder, recursive, extensions):
 			recursive = 'y'
 		if response.lower() == 'n':
 			recursive = 'n'
-			
+
 	# Initializes list of songs
 	song_list = []
-	
+
 	for extension in extensions:
 		# Finds all <extension> files in the directory, adds to list
 		if recursive == 'y':
@@ -70,12 +73,12 @@ def song_search(folder, recursive, extensions):
 			for file in os.listdir(folder):
 				if file.endswith(extension):
 					song_list.append(os.path.join(os.path.abspath(folder),file))
-				
+
 	# Return the song list
 	return song_list
 
-# Playlist creator function	
-def playlist_maker(songs_list,track_num,max_duration,max_filesize):
+# Playlist creator function
+def playlist_maker(songs_list,track_num,max_duration,max_filesize,genres_included=None):
 	while True:
 		# Initialize variables
 		random.shuffle(songs_list)
@@ -84,7 +87,17 @@ def playlist_maker(songs_list,track_num,max_duration,max_filesize):
 		duration = 0
 		last_track = 0
 		file_size = 0
-			
+
+        # Removes songs not matching genre inclusion criteria
+		if genres_included != None:
+			print(genres_included)
+			for song in songs_list:
+				tag = TinyTag.get(song)
+				print(tag.genre)
+				if tag.genre not in genres_included:
+					playlist.remove(song)
+					print('Removed ' + os.path.basename(song))
+						
 		# Limits playlist length based on filesizes
 		for song in playlist:
 			last_track += 1
@@ -93,9 +106,9 @@ def playlist_maker(songs_list,track_num,max_duration,max_filesize):
 			if file_size > max_filesize:
 				playlist = playlist[:last_track]
 				break
-				
+
 		last_track = 0
-		
+
 		# Limits playlist length based on duration
 		for song in playlist:
 			last_track += 1
@@ -104,20 +117,20 @@ def playlist_maker(songs_list,track_num,max_duration,max_filesize):
 			if duration > max_duration:
 				playlist = playlist[:last_track]
 				break
-		
+
 		last_track = 0
-		
+
 		# Limits playlist length based on number of tracks
 		if track_num != 0:
 			if track_num <= len(playlist):
 				playlist = playlist[:track_num]
-	
-		# Displays list of song names to verify list for user	
+
+		# Displays list of song names to verify list for user
 		for song in playlist:
 			name_list.append(os.path.basename(song))
 		pprint.pprint(name_list)
 		response = input('Is this playlist acceptable? [Y/N]')
-	
+
 		# Returns playlist if positive, redoes list if negative
 		if response.lower() == 'y':
 			return playlist
@@ -132,7 +145,8 @@ def playlist_writer(playlist_name,playlist):
 	for song in playlist:
 		playlist_file.write(song + '\n')
 	playlist_file.close()
-			
-songs = song_search(args.directory,args.query,args.file_extensions)
-playlist_made = playlist_maker(songs,args.track_nums,60*args.time_length,args.file_size)
-playlist_writer(args.playlist_name,playlist_made)
+
+	
+songs = song_search(args.d,args.query,args.ext)
+playlist_made = playlist_maker(songs,args.track_nums,60*args.time_length,args.file_size,args.genre_include)
+playlist_writer(args.playlist,playlist_made)
