dc2ac9db229939d183dc19091f5096310a79c25d
James Wash <jamescwash2@hotmail.com>
2018-05-07 20:47:13 -0400

    Added inclusion, exclusion scripting for albums, and titles

--- mixtape.py
+++ mixtape.py
@@ -45,9 +45,21 @@ parser.add_argument('-genre_exclude',type=str, nargs='*',
 # Artist inclusion arguments
 parser.add_argument('-artist_include',type = str, nargs='*',
 					help='Determines which artists should be included in the playlist. If an artist you want to include contains whitespace in the name, put the artist in quotes.')
-# Artist inclusion arguments
+# Artist exclusion arguments
 parser.add_argument('-artist_exclude',type = str, nargs='*',
 					help='Determines which artists should not be included in the playlist. If an artist you want to exclude contains whitespace in the name, put the artist in quotes.')
+# Album inclusion arguments
+parser.add_argument('-album_include',type = str, nargs='*',
+					help='Determines which albums should be included in the playlist. If an album you want to include contains whitespace in the name, put the album in quotes.')
+# Album exclusion arguments
+parser.add_argument('-album_exclude',type = str, nargs='*',
+					help='Determines which albums should not be included in the playlist. If an album you want to exclude contains whitespace in the name, put the album in quotes.')
+# Album inclusion arguments
+parser.add_argument('-title_include',type = str, nargs='*',
+					help='Determines which titles or keywords should be included in the playlist. If a title or keyword you want to include contains whitespace in the name, put the title or keyword in quotes.')
+# Album exclusion arguments
+parser.add_argument('-title_exclude',type = str, nargs='*',
+					help='Determines which titles or keywords should not be included in the playlist. If a title or keyword you want to exclude contains whitespace in the name, put the title or keyword in quotes.')					
 # Recursive search setting
 parser.add_argument('-query',default='q',choices = ['y','Y','n','N','q','Q'],
                     help='Determines if the program searches subfolders or not: Y - search subfolders | N - do not subfolders | Q - ask in program')
@@ -87,7 +99,7 @@ def song_search(folder, recursive, extensions):
 	return song_list
 
 # Playlist creator function
-def playlist_maker(songs_list,track_num,max_duration,max_filesize,genres_included,genres_blacklisted,artists_included,artists_blacklisted):
+def playlist_maker(songs_list,track_num,max_duration,max_filesize,genres_included,genres_blacklisted,artists_included,artists_blacklisted,albums_included,albums_blacklisted,titles_included,titles_blacklisted):
 	while True:
 		# Initialize variables
 		random.shuffle(songs_list)
@@ -140,6 +152,48 @@ def playlist_maker(songs_list,track_num,max_duration,max_filesize,genres_include
 			playlist = new_playlist
 			new_playlist = []
 			
+		# Removes songs not matching album inclusion criteria
+		if albums_included != None:
+			for album in albums_included:
+				for song in playlist:
+					tag = TinyTag.get(song)
+					if album in tag.album:
+						new_playlist.append(song)
+			playlist = new_playlist
+			new_playlist = []
+			
+		# Removes songs in blacklisted albums
+		if albums_blacklisted != None:
+			new_playlist = playlist.copy()
+			for album in albums_blacklisted:
+				for song in playlist:
+					tag = TinyTag.get(song)
+					if album in tag.album:
+						new_playlist.remove(song)
+			playlist = new_playlist
+			new_playlist = []
+			
+		# Removes songs not matching title inclusion criteria
+		if titles_included != None:
+			for title in titles_included:
+				for song in playlist:
+					tag = TinyTag.get(song)
+					if title in tag.title:
+						new_playlist.append(song)
+			playlist = new_playlist
+			new_playlist = []
+			
+		# Removes songs in blacklisted title
+		if titles_blacklisted != None:
+			new_playlist = playlist.copy()
+			for title in titles_blacklisted:
+				for song in playlist:
+					tag = TinyTag.get(song)
+					if title in tag.title:
+						new_playlist.remove(song)
+			playlist = new_playlist
+			new_playlist = []
+			
 		# Limits playlist length based on filesizes
 		for song in playlist:
 			last_track += 1
@@ -169,7 +223,8 @@ def playlist_maker(songs_list,track_num,max_duration,max_filesize,genres_include
 				
 		# Removes any songs that may have reoccured in the playlist
 		playlist = list(set(playlist))
-
+		random.shuffle(playlist)
+		
 		# Displays list of song names to verify list for user
 		for song in playlist:
 			name_list.append(os.path.basename(song))
@@ -190,8 +245,7 @@ def playlist_writer(playlist_name,playlist):
 	for song in playlist:
 		playlist_file.write(song + '\n')
 	playlist_file.close()
-
 	
 songs = song_search(args.d,args.query,args.ext)
-playlist_made = playlist_maker(songs,args.track_nums,60*args.time_length,args.file_size,args.genre_include,args.genre_exclude,args.artist_include,args.artist_exclude)
+playlist_made = playlist_maker(songs,args.track_nums,60*args.time_length,args.file_size,args.genre_include,args.genre_exclude,args.artist_include,args.artist_exclude,args.album_include,args.album_exclude,args.title_include,args.title_exclude)
 playlist_writer(args.playlist,playlist_made)
